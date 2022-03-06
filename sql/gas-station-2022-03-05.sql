6023bce5de9d6fed32415618ac5ff6472d1bd088
王子龙 <wzl709@outlook.com>
2022-03-06 09:45:03 +0800

    fix: 修正前端传来空参导致的异常

--- sql/gas-station-2022-03-05.sql
+++ sql/gas-station-2022-03-05.sql
@@ -13,27 +13,30 @@ CREATE TABLE `gas_station_info` (
   `city` varchar(32) NOT NULL COMMENT '加油站所在市',
   `isSystem` tinyint NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='加油站信息表';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
--- 正在导出表  ruoyi.gas_station_geo 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `gas_station_geo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `gas_station_geo` ENABLE KEYS */;
-
--- 导出  表 ruoyi.gas_station_info 结构
-CREATE TABLE IF NOT EXISTS `gas_station_info` (
-  `id` varchar(64) NOT NULL COMMENT '唯一ID',
-  `name` varchar(512) NOT NULL COMMENT '加油站名称',
-  `location` varchar(64) NOT NULL COMMENT '加油站位置',
-  `province` varchar(32) NOT NULL COMMENT '加油站所在省',
-  `city` varchar(32) NOT NULL COMMENT '加油站所在市',
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='加油站信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 -- 正在导出表  ruoyi.gas_station_info 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `gas_station_info` DISABLE KEYS */;
 /*!40000 ALTER TABLE `gas_station_info` ENABLE KEYS */;
 
+-- 导出  表 ruoyi.gas_station_geo 结构
+CREATE TABLE IF NOT EXISTS `gas_station_geo` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '地理信息主键',
+  `system_station_id` varchar(64) DEFAULT NULL COMMENT '系统内加油站ID',
+  `out_system_station_id` varchar(64) DEFAULT NULL COMMENT '系统外加油站ID',
+  `distance` double DEFAULT NULL COMMENT '两加油站之间的距离',
+  `traffic_lights` int DEFAULT NULL COMMENT '两加油站之间路线红绿灯数',
+  `traffic_factor` double DEFAULT NULL COMMENT '两加油站红绿灯数的影响因子',
+  `route_shape` varchar(512) DEFAULT NULL COMMENT '路线形状',
+  `route_shape_factor` double DEFAULT NULL COMMENT '路线曲折度影响系数',
+  `route_factor` double DEFAULT NULL COMMENT '路线影响系数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='加油站地理信息表';
+
+-- 正在导出表  ruoyi.gas_station_geo 的数据：~0 rows (大约)
+    /*!40000 ALTER TABLE `gas_station_geo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gas_station_geo` ENABLE KEYS */;
 -- 正在导出表  ruoyi.sys_menu 的数据：~83 rows (大约)
 /*!40000 ALTER TABLE `sys_menu` DISABLE KEYS */;
 INSERT INTO `sys_menu` (`menu_id`, `menu_name`, `parent_id`, `order_num`, `path`, `component`, `query`, `is_frame`, `is_cache`, `menu_type`, `visible`, `status`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES
