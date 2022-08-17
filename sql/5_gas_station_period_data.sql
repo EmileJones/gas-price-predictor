1606919c1d62f1dde6f7d497070839ee8a82ecd9
KlenKiven <wzl709@outlook.com>
2022-08-17 15:48:18 +0800

    DEPLOY 部署修正

--- sql/5_gas_station_period_data.sql
+++ sql/5_gas_station_period_data.sql
@@ -1,5 +1,7 @@
 insert into `gas_station_price_period`(`start_time`, `rise`)
-values ('2022-07-13', false),
+values ('2022-08-10', false),
+       ('2022-07-27', false),
+       ('2022-07-13', false),
        ('2022-06-29', false),
        ('2022-06-15', true),
        ('2022-05-31', true),
